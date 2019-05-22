b178894e87145651fae54ea1ce2f58c120d1d120
Weeya <weeyaa.w@gmail.com>
2019-05-22 16:37:21 +0700

    Updates

---
+++ precache-manifest.ebd6726f4043b09ecd56818857130c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b97f503aa6bda7443ada2a988255707",
+    "url": "/statvis/index.html"
+  },
+  {
+    "revision": "91de29143331e5deaeeb",
+    "url": "/statvis/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "26ca586dd9e4da8121d0",
+    "url": "/statvis/static/js/2.7901da73.chunk.js"
+  },
+  {
+    "revision": "91de29143331e5deaeeb",
+    "url": "/statvis/static/js/main.ca651844.chunk.js"
+  },
+  {
+    "revision": "e55ea8dddf9d79366c85",
+    "url": "/statvis/static/js/runtime~main.645b05d2.js"
+  }
+]);
